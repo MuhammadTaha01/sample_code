bedc48b8f8e6ce2888580c00c2c806d47ff938a5
Muhammad Taha <tahafaisal9292@gmail.com>
2025-02-07 00:41:42 +0500

    calling the api key directly from .env file was enabled

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from '@clerk/clerk-react'; // Remove RedirectToSignIn i
 import App from './App.jsx';
 import './index.css';
 
-const PUBLISHABLE_KEY = 'pk_test_c2VjdXJlLXR1cnRsZS02Mi5jbGVyay5hY2NvdW50cy5kZXYk';
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
